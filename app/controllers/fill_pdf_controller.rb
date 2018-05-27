5524b2981e289b12dd1737abbf075972c15cd051
jolavb <bartholdjulian@gmail.com>
2018-05-27 18:32:37 -0400

    Add fill_pdf controller and GemFile
    using fillable-PDF: https://github.com/vkononov/fillable-pdf
    to grab fillable fields in a PDF, fill them out
    and spit out a new pdf.

---
+++ app/controllers/fill_pdf_controller.rb
@@ -0,0 +1,15 @@
+
+require 'fillable-pdf'
+class FillPdfController < ApplicationController
+
+
+  def show
+    pdf = FillablePDF.new './public/pdf-templates/Form1.pdf'
+    pdf.set_fields("Given Name Text Box": 'Julian', "Family Name Text Box": 'Barthold')
+    pdf.save_as('./public/pdf-output/output.pdf')
+    render  html:   '<div>html Document Saved</div>'.html_safe
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pdf', action: :show, controller: 'fill_pdf';
+  
 end

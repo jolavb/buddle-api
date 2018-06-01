9208694c04e3332e9700322bc12608a561eb531a
jolavb <bartholdjulian@gmail.com>
2018-06-01 12:02:57 -0400

    Add some comments to test mailer

--- app/controllers/fill_pdf_controller.rb
+++ app/controllers/fill_pdf_controller.rb
@@ -4,11 +4,10 @@ class FillPdfController < ApplicationController
 
 
   def show
-    ConfirmAccountMailer.welcome_email(User.first).deliver
-    # pdf = FillablePDF.new './public/pdf-templates/Form1.pdf'
-    # pdf.set_fields("Given Name Text Box": 'Julian', "Family Name Text Box": 'Barthold')
-    # pdf.save_as('./public/pdf-output/output.pdf')
-    # render  html:   '<div>html Document Saved</div>'.html_safe
+    pdf = FillablePDF.new './public/pdf-templates/Form1.pdf'
+    pdf.set_fields("Given Name Text Box": 'Julian', "Family Name Text Box": 'Barthold')
+    pdf.save_as('./public/pdf-output/output.pdf')
+    render  html:   '<div>html Document Saved</div>'.html_safe
   end
 
 
--- app/mailers/confirm_account_mailer.rb
+++ app/mailers/confirm_account_mailer.rb
@@ -1,4 +1,7 @@
 class ConfirmAccountMailer < ApplicationMailer
+
+# This is a test mailer to make sure email is setup correctly
+# Use the following to call this method from a controller: ConfirmAccountMailer.welcome_email(User.first).deliver
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome!')
